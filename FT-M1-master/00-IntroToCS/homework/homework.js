a5b33ba17f604709fdac2248a845cebbca3fd44b
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-08-02 20:43:17 -0500

    homework

--- FT-M1-master/00-IntroToCS/homework/homework.js
+++ FT-M1-master/00-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let numero = 0;
+  let array = num.split("").reverse();  
+       for(let i = 0; i < array.length; i++){
+          numero += (array[i]) * (2 ** i) 
+  }
+  return numero;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let almacenado = [];
+  while(num > 0 ){
+   almacenado.unshift(num % 2)
+   num = Math.floor(num / 2)
+   num
+  }
+return almacenado.join("")
 }
 
 
