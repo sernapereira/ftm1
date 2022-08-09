676c953b72d24c9c987c489a030d00c0e403031f
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-08-09 00:01:41 -0500

    guardado

--- FT-M1-master/04-EstructuraDeDatos-II/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-II/homework/homework.js
@@ -11,12 +11,64 @@
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
 function LinkedList() {
-
+  this.head = null
+  this._length
 }
 
 function Node(value){
-
+  this.value = value
+  this.next = null
 }
+LinkedList.prototype.add = function(){
+  let node = new Node();
+   let current = this.head
+      if(!current){
+          this.head = node
+          return node;
+       }
+  
+       while (current.next !== null){
+              current = current.next
+       }
+      
+      current.next = node;
+      this._length++;
+      return node
+  }
+  
+  LinkedList.prototype.remove = function(){
+      if(this.head === null){
+          return null;
+  
+      }else if(this.head && !this.head.next){
+      let aux = this.head.value; 
+      this.head = null;
+      this._length--;
+      return aux;
+      }
+  
+      while(current.next.next){
+          current = current.next;
+      }
+      let aux = current.next.value;
+      current.next = null;
+      this._length--;
+      return aux;
+  };
+  LinkedList.prototype.search = function(value){
+      if(this.head === null)return null;
+      let current = this.head;
+      while(current){
+          if(current.value === value)return current.value;
+          else if(typeof value === function){
+              if (value(current.value)){
+                  return current.value;
+              }
+          }
+          current = current.next;
+      }
+      return null;
+  };
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
 // Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
@@ -31,8 +83,11 @@ function Node(value){
 //    - Retornar dicho valor.
 
 function HashTable() {
-
+  this.memory;
 }
+HashTable.prototype.set = function (){}
+HashTable.prototype.get = function (){};
+HashTable.prototype.hasKey = function (){};
 
 
 // No modifiquen nada debajo de esta linea
--- FT-M1-master/05-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/05-EstructuraDeDatos-III/homework/homework.js
@@ -8,9 +8,78 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
 
-}
+BinarySearchTree.prototype.insert = function(valor){
+      if(valor > this.value){
+          if(!this.right){
+              this.right = new BinarySearchTree(valor);
+          }else{
+              this.right.insert(valor);
+          }
+      }else{
+          if(!this.left){
+              this.left = new BinarySearchTree(valor);
+          }else{
+              this.left.insert(valor)
+          }
+      } 
+      
+};
+
+
+BinarySearchTree.prototype.contains = function(valorBuscado){
+  if(this.value === valorBuscado)return true;
+           if(valorBuscado > this.value){
+           if(!this.right)return false;
+           else return this.right.contains(valorBuscado);
+  }else{
+           if(!this.left)return false;
+           else return this.left.contains(valorBuscado);
+  }
+};
+
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, metodo){
+  if(!metodo || metodo === `in-order`){
+     if(this.left)this.left.depthFirstForEach(cb,metodo)
+     cb(this.value)
+     if(this.right)this.right.depthFirstForEach(cb,metodo)
+
+
+  }else if(metodo === `pre-order`){
+    cb(this.value)
+    if(this.left)this.left.depthFirstForEach(cb,metodo)
+    if(this.right)this.right.depthFirstForEach(cb,metodo)
+  
+  
+  }else if(metodo === `post-order`){
+    if(this.left)this.left.depthFirstForEach(cb,metodo)
+    if(this.right)this.right.depthFirstForEach(cb,metodo)
+    cb(this.value)
+  }
+};
+
+
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array){
+  if(!array) var array = [];
+  if(this.left)array.push(this.left);
+  if(this.right)array.push(this.right);
+  cb(this.value);
+  if(array.length > 0)array.shift().breadthFirstForEach(cb, array)
+};
+
+BinarySearchTree.prototype.size = function(){
+  if(!this.left && !this.right)return 1;
+  if(this.left && this.right) return 1 + this.left.size() + this.right.size();
+  if(this.left)return 1 + this.left.size();
+  if(this.right)return 1 + this.right.size();
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
